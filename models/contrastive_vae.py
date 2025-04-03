5ff50596f09d27201517804b5ab2b889c9cf1033
Alex Lin <allx2100@gmail.com>
2025-04-02 22:23:50 -0500

    reorganize

--- models/contrastive_vae.py
+++ models/contrastive_vae.py
@@ -1,11 +1,29 @@
 import torch
-import torch.nn
+import torch.nn as nn
 
 
 class CVAE(nn.module):
-    def __init__(self, input_channels, image_size, latent_dims, channels, kld_weight):
+    def __init__(self, input_channels, image_size, latent_dims, channels, kld_weight,encoderlayers):
         super(CVAE, self).__init__()
         self.input_layer = image_size
         self.encoder_net = nn.Sequential(
-            nn.Convolution()
+            nn.Conv2d(input_channels, channels[0], 4, stride=2,padding=1),
+            nn.ReLU(),
+            nn.Conv2d(channels[0],channels[1],4,stride=2,padding=1),
+            nn.ReLU()
         )
+        self.encoder = nn.Sequential(encoderlayers)
+    def encode(self,x):
+        x = self.encoder(x)
+        return x
+
+    def decode():
+        pass
+    def forward(data):
+        encoded = self.encoder_net
+        encoded = encoded.flatten()
+        
+
+
+
+
